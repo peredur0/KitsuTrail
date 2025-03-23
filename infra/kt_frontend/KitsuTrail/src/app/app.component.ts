3a2b11ababe7901b2f31155fe90be583e5dfad17
peredur0 <martial.goehry@gmail.com>
2025-03-24 00:08:00 +0100

    Continuation on base architecture Inserted: - landing page - users list
    Added some routing, and starting to put some links in the sidebar

--- infra/kt_frontend/KitsuTrail/src/app/app.component.ts
+++ infra/kt_frontend/KitsuTrail/src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/components/header/header.component';
+import { SidebarComponent } from './core/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    HeaderComponent,
+    SidebarComponent,
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- infra/kt_frontend/KitsuTrail/src/app/app.routes.ts
+++ infra/kt_frontend/KitsuTrail/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './core/components/landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'users', loadChildren: () => import('./features/users/users.routes').then(m => m.usersRoutes)},
+    { path: '', component: LandingPageComponent }
+];
