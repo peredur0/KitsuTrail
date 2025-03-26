85f494a5ccda5e30e85782da33af22ede1269a29
peredur0 <martial.goehry@gmail.com>
2025-03-26 08:57:13 +0100

    static version of landing page

--- infra/kt_frontend/Shiro/src/app/core/components/landing-page/landing-page.component.ts
+++ infra/kt_frontend/Shiro/src/app/core/components/landing-page/landing-page.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuService } from '../../services/menu.service';
+import { Menu } from '../../models/menu.model';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterLink
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit{
+  
+  menu!: Menu[];
+  constructor(private menuService: MenuService){}
 
+
+  ngOnInit(): void {
+    this.menu = this.menuService.getMenu();
+  }
 }
