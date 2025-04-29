fa83d75fd6cd659876778a4344e4c3267cf910f6
peredur0 <martial.goehry@gmail.com>
2025-04-30 00:21:12 +0200

    Creation of providers lists components

---
+++ infra/kt_frontend/Tenko/src/app/features/providers/components/identities-list/identities-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-identities-list',
+  imports: [],
+  templateUrl: './identities-list.component.html',
+  styleUrl: './identities-list.component.scss'
+})
+export class IdentitiesListComponent {
+
+}
