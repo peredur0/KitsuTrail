2f999a32b6da9e2e4e4e7dc2ff98b93426a7ca37
peredur0 <martial.goehry@gmail.com>
2025-05-04 18:06:09 +0200

    added providers in sqlite table

---
+++ infra/kt_database/sqlite/init_providers_table.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+"""
+Init the providers table in SQLite DB
+"""
+
+import json
+import sqlite3
+
+PROVIDERS_TABLE = 'providers'
+
+
+if __name__ == '__main__':
+    print('Init of dev database - step providers')
+    connection = sqlite3.connect('inari.db')
+    cursor = connection.cursor()
+    cursor.execute(f'DROP TABLE IF EXISTS {PROVIDERS_TABLE}')
+    cursor.execute(f'''
+        CREATE TABLE IF NOT EXISTS {PROVIDERS_TABLE}(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            type TEXT NOT NULL,
+            protocol TEXT NOT NULL,
+            name TEXT NOT NULL
+        )
+    ''')
+
+    cursor.execute('CREATE INDEX idx_type ON providers(type)')
+    cursor.execute('CREATE INDEX idx_protocol ON providers(protocol)')
+    cursor.execute('CREATE INDEX idx_name ON providers(name)')
+
+    with open('./dev_base_providers.json', 'r') as file:
+        data = json.load(file)
+        for provider in data:
+            cursor.execute(f'''
+                INSERT INTO {PROVIDERS_TABLE} (type, protocol, name)
+                VALUES (:type, :protocol, :name)
+            ''', provider)
+    
+    connection.commit()
+    connection.close()
+    print('Init dev database - step providers: end')
