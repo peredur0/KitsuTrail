a0b0a0f643490a6a996855aac4260aa7bcf2bed5
peredur0 <martial.goehry@gmail.com>
2025-06-04 08:04:30 +0200

    removed ng2-charts and charts.js

--- infra/kt_frontend/Tenko/src/app/features/stats/components/charts/bar-providers-success-fail/bar-providers-success-fail.component.ts
+++ infra/kt_frontend/Tenko/src/app/features/stats/components/charts/bar-providers-success-fail/bar-providers-success-fail.component.ts
@@ -1,34 +1,11 @@
 import { Component } from '@angular/core';
 
-import { ChartConfiguration } from 'chart.js';
-import { BaseChartDirective } from 'ng2-charts';
-
 @Component({
   selector: 'app-bar-providers-success-fail',
-  imports: [
-    BaseChartDirective
-  ],
+  imports: [],
   templateUrl: './bar-providers-success-fail.component.html',
   styleUrl: './bar-providers-success-fail.component.scss'
 })
 export class BarProvidersSuccessFailComponent {
   // @Input() data!: ???;
-
-  public barChartData: ChartConfiguration<'bar'>['data'] = {
-    labels: ['prov1', 'prov2', 'prov3'],
-    datasets: [
-      { data: [3, 5, 8], label: 'Success'},
-      { data: [4, 2, 0], label: 'Fail'}
-    ]
-  }
-
-  public barChartOptions: ChartConfiguration<'bar'>['options'] = {
-    indexAxis: 'y',
-    responsive: true,
-    plugins: { legend: { display: true }},
-    scales: {
-      x: { beginAtZero: true },
-      y: { beginAtZero: true }
-    }
-  };
 }
--- infra/kt_frontend/Tenko/src/app/features/stats/components/charts/line-timestamp-auth-access/line-timestamp-auth-access.component.ts
+++ infra/kt_frontend/Tenko/src/app/features/stats/components/charts/line-timestamp-auth-access/line-timestamp-auth-access.component.ts
@@ -1,49 +1,20 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
-import { ChartConfiguration } from 'chart.js';
-import { BaseChartDirective } from 'ng2-charts';
-
 import { UsersActivities } from '../../../models/stats.model';
 
 @Component({
   selector: 'app-line-timestamp-auth-access',
-  imports: [
-    BaseChartDirective
-  ],
+  imports: [],
   templateUrl: './line-timestamp-auth-access.component.html',
   styleUrl: './line-timestamp-auth-access.component.scss'
 })
 export class LineTimestampAuthAccessComponent implements OnChanges {
   @Input() data!: UsersActivities
-  
-  public lineChartData!: ChartConfiguration['data']
-  public lineChartOptions: ChartConfiguration['options'] = {
-    elements: { line: { tension: 0.3 }},
-    scales: { y: { position: 'left' }},
-    plugins: { legend: { display: true }}
-  };
 
   ngOnChanges(): void {
     if (!this.data) return;
-    
-    this.lineChartData = {
-      datasets: [
-        {
-          data: this.data.authentications.data,
-          label: "Authentifications",
-          borderColor: 'rgba(0, 100, 0, 1)',
-          backgroundColor: 'rgba(0, 100, 0, 0.2)',
-          fill: 'origin'
-        },
-        {
-          data: this.data.access.data,
-          label: "Accès",
-          borderColor: 'rgba(0, 0, 139, 1)',
-          backgroundColor: 'rgba(0, 0, 139, 0.2)',
-          fill: 'origin'
-        }
-      ],
-      labels: this.data.labels.data
-    };
+    console.log(this.data.authentications.data);
+    console.log(this.data.access.data);
+    console.log(this.data.labels.data);
   }
 }
